903b0c6b8a6ef68aa8e35e191cac2f93198e11ab
Alfie <djobbo.maiga@gmail.com>
2021-01-28 01:16:03 +0100

    sw

--- sw.js
+++ sw.js
@@ -1,78 +1,301 @@
-// Name of the cache
-const CACHE_NAME = 'cache';
-// Caching duration of the items, one week here
-const CACHING_DURATION = 7 * 24 * 3600;
-// Verbose logging or not
-const DEBUG = true;
+const CACHE_VERSION = 1;
 
-globalThis.addEventListener('fetch', (event) => {
-	const { request } = event;
+const BASE_CACHE_FILES = [
+	'/main.css',
+	'/app.js',
+	'/manifest.json',
+	'/icons/favicon.png',
+];
 
-	// ...
+const OFFLINE_CACHE_FILES = ['/main.css', '/app.js', '/index.html'];
 
-	event.respondWith(
-		globalThis.caches.open(`${CACHE_NAME}-tiles`).then((cache) =>
-			cache.match(request).then((response) => {
-				// If there is a match from the cache
-				if (response) {
-					DEBUG &&
-						console.log(`SW: serving ${request.url} from cache.`);
-					const expirationDate = Date.parse(
-						response.headers.get('sw-cache-expires')
-					);
-					const now = new Date();
-					// Check it is not already expired and return from the
-					// cache
-					if (expirationDate > now) {
-						return response;
-					}
+const NOT_FOUND_CACHE_FILES = ['/main.css', '/app.js', '/index.html'];
+
+const OFFLINE_PAGE = '/index.html';
+const NOT_FOUND_PAGE = '/index.html';
+
+const CACHE_VERSIONS = {
+	assets: 'assets-v' + CACHE_VERSION,
+	content: 'content-v' + CACHE_VERSION,
+	offline: 'offline-v' + CACHE_VERSION,
+	notFound: '404-v' + CACHE_VERSION,
+};
+
+// Define MAX_TTL's in SECONDS for specific file extensions
+const MAX_TTL = {
+	'/': 3600,
+	html: 3600,
+	json: 86400,
+	js: 86400,
+	css: 86400,
+};
+
+const CACHE_BLACKLIST = [
+	//(str) => {
+	//    return !str.startsWith('http://localhost') && !str.startsWith('https://gohugohq.com');
+	//},
+];
+
+const SUPPORTED_METHODS = ['GET'];
+
+/**
+ * isBlackListed
+ * @param {string} url
+ * @returns {boolean}
+ */
+function isBlacklisted(url) {
+	return CACHE_BLACKLIST.length > 0
+		? !CACHE_BLACKLIST.filter((rule) => {
+				if (typeof rule === 'function') {
+					return !rule(url);
+				} else {
+					return false;
 				}
+		  }).length
+		: false;
+}
 
-				// Otherwise, let's fetch it from the network
-				DEBUG &&
-					console.log(
-						`SW: no match in cache for ${request.url}, using network.`
-					);
-				// Note: We HAVE to use fetch(request.url) here to ensure we
-				// have a CORS-compliant request. Otherwise, we could get back
-				// an opaque response which we cannot inspect
-				// (https://developer.mozilla.org/en-US/docs/Web/API/Response/type).
-				return fetch(request.url).then((liveResponse) => {
-					// Compute expires date from caching duration
-					const expires = new Date();
-					expires.setSeconds(expires.getSeconds() + CACHING_DURATION);
-					// Recreate a Response object from scratch to put
-					// it in the cache, with the extra header for
-					// managing cache expiration.
-					const cachedResponseFields = {
-						status: liveResponse.status,
-						statusText: liveResponse.statusText,
-						headers: { 'SW-Cache-Expires': expires.toUTCString() },
-					};
-					liveResponse.headers.forEach((v, k) => {
-						cachedResponseFields.headers[k] = v;
-					});
-					// We will consume body of the live response, so
-					// clone it before to be able to return it
-					// afterwards.
-					const returnedResponse = liveResponse.clone();
-					return liveResponse.blob().then((body) => {
-						DEBUG &&
-							console.log(
-								`SW: caching tiles ${
-									request.url
-								} until ${expires.toUTCString()}.`
-							);
-						// Put the duplicated Response in the cache
-						cache.put(
-							request,
-							new Response(body, cachedResponseFields)
-						);
-						// Return the live response from the network
-						return returnedResponse;
-					});
-				});
+/**
+ * getFileExtension
+ * @param {string} url
+ * @returns {string}
+ */
+function getFileExtension(url) {
+	let extension = url.split('.').reverse()[0].split('?')[0];
+	return extension.endsWith('/') ? '/' : extension;
+}
+
+/**
+ * getTTL
+ * @param {string} url
+ */
+function getTTL(url) {
+	if (typeof url === 'string') {
+		let extension = getFileExtension(url);
+		if (typeof MAX_TTL[extension] === 'number') {
+			return MAX_TTL[extension];
+		} else {
+			return null;
+		}
+	} else {
+		return null;
+	}
+}
+
+/**
+ * installServiceWorker
+ * @returns {Promise}
+ */
+function installServiceWorker() {
+	return Promise.all([
+		caches.open(CACHE_VERSIONS.assets).then((cache) => {
+			return cache.addAll(BASE_CACHE_FILES);
+		}),
+		caches.open(CACHE_VERSIONS.offline).then((cache) => {
+			return cache.addAll(OFFLINE_CACHE_FILES);
+		}),
+		caches.open(CACHE_VERSIONS.notFound).then((cache) => {
+			return cache.addAll(NOT_FOUND_CACHE_FILES);
+		}),
+	]).then(() => {
+		return self.skipWaiting();
+	});
+}
+
+/**
+ * cleanupLegacyCache
+ * @returns {Promise}
+ */
+function cleanupLegacyCache() {
+	let currentCaches = Object.keys(CACHE_VERSIONS).map((key) => {
+		return CACHE_VERSIONS[key];
+	});
+
+	return new Promise((resolve, reject) => {
+		caches
+			.keys()
+			.then((keys) => {
+				return (legacyKeys = keys.filter((key) => {
+					return !~currentCaches.indexOf(key);
+				}));
 			})
-		)
+			.then((legacy) => {
+				if (legacy.length) {
+					Promise.all(
+						legacy.map((legacyKey) => {
+							return caches.delete(legacyKey);
+						})
+					)
+						.then(() => {
+							resolve();
+						})
+						.catch((err) => {
+							reject(err);
+						});
+				} else {
+					resolve();
+				}
+			})
+			.catch(() => {
+				reject();
+			});
+	});
+}
+
+function precacheUrl(url) {
+	if (!isBlacklisted(url)) {
+		caches.open(CACHE_VERSIONS.content).then((cache) => {
+			cache
+				.match(url)
+				.then((response) => {
+					if (!response) {
+						return fetch(url);
+					} else {
+						// already in cache, nothing to do.
+						return null;
+					}
+				})
+				.then((response) => {
+					if (response) {
+						return cache.put(url, response.clone());
+					} else {
+						return null;
+					}
+				});
+		});
+	}
+}
+
+self.addEventListener('install', (event) => {
+	event.waitUntil(Promise.all([installServiceWorker(), self.skipWaiting()]));
+});
+
+// The activate handler takes care of cleaning up old caches.
+self.addEventListener('activate', (event) => {
+	event.waitUntil(
+		Promise.all([
+			cleanupLegacyCache(),
+			self.clients.claim(),
+			self.skipWaiting(),
+		]).catch((err) => {
+			event.skipWaiting();
+		})
 	);
+});
+
+self.addEventListener('fetch', (event) => {
+	event.respondWith(
+		caches.open(CACHE_VERSIONS.content).then((cache) => {
+			return cache
+				.match(event.request)
+				.then((response) => {
+					if (response) {
+						let headers = response.headers.entries();
+						let date = null;
+
+						for (let pair of headers) {
+							if (pair[0] === 'date') {
+								date = new Date(pair[1]);
+							}
+						}
+
+						if (date) {
+							let age = parseInt(
+								(new Date().getTime() - date.getTime()) / 1000
+							);
+							let ttl = getTTL(event.request.url);
+
+							if (ttl && age > ttl) {
+								return new Promise((resolve) => {
+									return fetch(event.request.clone())
+										.then((updatedResponse) => {
+											if (updatedResponse) {
+												cache.put(
+													event.request,
+													updatedResponse.clone()
+												);
+												resolve(updatedResponse);
+											} else {
+												resolve(response);
+											}
+										})
+										.catch(() => {
+											resolve(response);
+										});
+								}).catch((err) => {
+									return response;
+								});
+							} else {
+								return response;
+							}
+						} else {
+							return response;
+						}
+					} else {
+						return null;
+					}
+				})
+				.then((response) => {
+					if (response) {
+						return response;
+					} else {
+						return fetch(event.request.clone())
+							.then((response) => {
+								if (response.status < 400) {
+									if (
+										~SUPPORTED_METHODS.indexOf(
+											event.request.method
+										) &&
+										!isBlacklisted(event.request.url)
+									) {
+										cache.put(
+											event.request,
+											response.clone()
+										);
+									}
+									return response;
+								} else {
+									return caches
+										.open(CACHE_VERSIONS.notFound)
+										.then((cache) => {
+											return cache.match(NOT_FOUND_PAGE);
+										});
+								}
+							})
+							.then((response) => {
+								if (response) {
+									return response;
+								}
+							})
+							.catch(() => {
+								return caches
+									.open(CACHE_VERSIONS.offline)
+									.then((offlineCache) => {
+										return offlineCache.match(OFFLINE_PAGE);
+									});
+							});
+					}
+				})
+				.catch((error) => {
+					console.error('  Error in fetch handler:', error);
+					throw error;
+				});
+		})
+	);
+});
+
+self.addEventListener('message', (event) => {
+	if (
+		typeof event.data === 'object' &&
+		typeof event.data.action === 'string'
+	) {
+		switch (event.data.action) {
+			case 'cache':
+				precacheUrl(event.data.url);
+				break;
+			default:
+				console.log('Unknown action: ' + event.data.action);
+				break;
+		}
+	}
 });
